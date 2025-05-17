8d9dfa385dfdf14fa40b3fb3cea72a8239c475ab
WDTI0\muhammad.alhamdani <muhammad.alhamdani@primecash.co.id>
2025-05-17 11:32:37 +0700

    add unit

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,9 @@ Blocks.heatSource.buildVisibility = BuildVisibility.shown;
 require('super-cheat/chrono-unloader')
 require('super-cheat/chrono-pusher')
 require('super-cheat/next-wave')
+
+const units = [UnitTypes.alpha,UnitTypes.beta,UnitTypes.gamma];
+units.forEach(e=>e.abilities.add(UnitTypes.renale.abilities.get(0)));
+UnitTypes.alpha.abilities.get(0).percentAmount = 0.0417 * 1000;
+UnitTypes.beta.abilities.get(0).percentAmount = 0.0556 * 1000;
+UnitTypes.gamma.abilities.get(0).percentAmount = 0.0834 * 1000;
